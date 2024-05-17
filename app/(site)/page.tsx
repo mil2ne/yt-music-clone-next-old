98988f7602cc7798e3f2a676782ee9be7cb6ee6b
celee <show3070@gmail.com>
2024-05-18 05:56:08 +0900

    #8. 더미데이터
    클론에 필요한 더미데이터 우선 생성

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,11 +5,11 @@ const page = async () => {
   // await sleep(2000);
   // throw new Error("my error");
 
-  console.log("before Homepage sleep ...");
+  // console.log("before Homepage sleep ...");
 
-  await sleep(4000);
+  // await sleep(4000);
 
-  console.log("after Homepage sleep ...");
+  // console.log("after Homepage sleep ...");
 
   return <div>Home Page</div>;
 };
