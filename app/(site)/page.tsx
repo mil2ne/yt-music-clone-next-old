eeae68fa9b4fc0808ecd44ade01a084edb4efb7c
celee <show3070@gmail.com>
2024-05-17 14:12:54 +0900

    #7. rootLayout 에서 피해야 할 것
    RootLayout의 지연은 전체 App의 느려지는 현상으로 번진다.
    
    case1
    root 4s + home 2s - 4s
    case2
    root 2s + home 4s -> 2s -> home loading... 2s -> home page
    case3
    root 4s + home 2s - 4s -> home page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,6 +4,13 @@ import React from "react";
 const page = async () => {
   // await sleep(2000);
   // throw new Error("my error");
+
+  console.log("before Homepage sleep ...");
+
+  await sleep(4000);
+
+  console.log("after Homepage sleep ...");
+
   return <div>Home Page</div>;
 };
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/providers/themeProvider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { sleep } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
