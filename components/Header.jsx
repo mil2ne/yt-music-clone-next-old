c5a1f7c3b845565c389e565f10390c0989dadb6d
celee <show3070@gmail.com>
2024-05-19 11:23:28 +0900

    #15 - Header-bg
    헤더에 백그라운드 작업

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
 import React from "react";
 
 const Header = ({ children }) => {
-  return <header>{children}</header>;
+  return (
+    <header className=" relative overflow-y-auto w-full h-full">
+      <section className=" absolute top-0 w-full">
+        <div className=" relative h-[400px] w-full">
+          <Image
+            alt="mediaItem"
+            className="object-cover"
+            fill
+            src="https://images.unsplash.com/photo-1707833558984-3293e794031c"
+          />
+          <div className=" absolute h-[400]px top-0 bg-black opacity-40 w-full h-full"></div>
+          <div className=" absolute h-[400]px top-0 bg-gradient-to-t from-black w-full h-full"></div>
+        </div>
+      </section>
+      <section className=" absolute">{children}</section>
+    </header>
+  );
 };
 
 export default Header;
