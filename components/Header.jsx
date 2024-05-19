70897602217453e32ba64bc74c12d2fb4f0dbf4a
celee <show3070@gmail.com>
2024-05-19 11:31:44 +0900

    #16 - shadcn cli
    avatar 생성

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import UserIcon from "@/components/UserIcon";
 
 const Header = ({ children }) => {
   return (
@@ -15,6 +16,9 @@ const Header = ({ children }) => {
           <div className=" absolute h-[400]px top-0 bg-black opacity-40 w-full h-full"></div>
           <div className=" absolute h-[400]px top-0 bg-gradient-to-t from-black w-full h-full"></div>
         </div>
+      </section>
+      <section>
+        <UserIcon />
       </section>
       <section className=" absolute">{children}</section>
     </header>
